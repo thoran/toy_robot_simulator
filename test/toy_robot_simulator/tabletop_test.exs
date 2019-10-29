c2b4b27e591e1efa17d0353b122226fdf47502e7
thoran <code@thoran.com>
2019-10-30 09:24:44 +1100

    ~ ToyRobotSimulator.Tabletop test, so as to demonstrate correctness when a location is not valid.

--- test/toy_robot_simulator/tabletop_test.exs
+++ test/toy_robot_simulator/tabletop_test.exs
@@ -4,27 +4,43 @@ defmodule ToyRobotSimulator.TabletopTest do
   doctest ToyRobotSimulator.Tabletop
   alias ToyRobotSimulator.Tabletop
 
-  describe "valid_location() when only x is supplied" do
-    test "when a valid location" do
+  describe "valid_location?() is true" do
+    test "when only x is supplied" do
       location = %{x: 0}
       result = location |> Tabletop.valid_location?
       assert result == true
     end
-  end
 
-  describe "valid_location() when only y is supplied" do
-    test "when a valid location" do
+    test "when only y is supplied" do
       location = %{y: 0}
+      result = location |> Tabletop.valid_location?
+      assert result == true
+    end
+
+    test "when both x and y are supplied" do
+      location = %{x: 0, y: 0}
       result = location |> Tabletop.valid_location?
       assert result == true
     end
   end
 
-  describe "valid_location() when both x and y are supplied" do
-    test "when a valid location" do
-      location = %{x: 0, y: 0}
+  describe "valid_location?() is false" do
+    test "when only x is supplied" do
+      location = %{x: 5}
       result = location |> Tabletop.valid_location?
-      assert result == true
+      assert result == false
+    end
+
+    test "when only y is supplied" do
+      location = %{y: 5}
+      result = location |> Tabletop.valid_location?
+      assert result == false
+    end
+
+    test "when both x and y are supplied" do
+      location = %{x: 5, y: 5}
+      result = location |> Tabletop.valid_location?
+      assert result == false
     end
   end
 
