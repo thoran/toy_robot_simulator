5cb88ab608c5e36b31f00c967b561b0d50c676bd
thoran <code@thoran.com>
2019-10-29 17:33:12 +1100

    ~ ToyRobotSimulator.ToyRobotTest to make full use of the alias.

--- test/toy_robot_simulator/toy_robot_test.exs
+++ test/toy_robot_simulator/toy_robot_test.exs
@@ -6,25 +6,25 @@ defmodule ToyRobotSimulator.ToyRobotTest do
 
   describe ".move()" do
     test "when facing East" do
-      toy_robot = %ToyRobotSimulator.ToyRobot{x: 0, y: 0, facing: :east}
+      toy_robot = %ToyRobot{x: 0, y: 0, facing: :east}
       toy_robot = toy_robot |> ToyRobot.move
       assert toy_robot == %ToyRobot{x: 1, y: 0, facing: :east}
     end
 
     test "when facing West" do
-      toy_robot = %ToyRobotSimulator.ToyRobot{x: 1, y: 0, facing: :west}
+      toy_robot = %ToyRobot{x: 1, y: 0, facing: :west}
       toy_robot = toy_robot |> ToyRobot.move
       assert toy_robot == %ToyRobot{x: 0, y: 0, facing: :west}
     end
 
     test "when facing North" do
-      toy_robot = %ToyRobotSimulator.ToyRobot{x: 0, y: 0, facing: :north}
+      toy_robot = %ToyRobot{x: 0, y: 0, facing: :north}
       toy_robot = toy_robot |> ToyRobot.move
       assert toy_robot == %ToyRobot{x: 0, y: 1, facing: :north}
     end
 
     test "when facing South" do
-      toy_robot = %ToyRobotSimulator.ToyRobot{x: 0, y: 1, facing: :south}
+      toy_robot = %ToyRobot{x: 0, y: 1, facing: :south}
       toy_robot = toy_robot |> ToyRobot.move
       assert toy_robot == %ToyRobot{x: 0, y: 0, facing: :south}
     end
