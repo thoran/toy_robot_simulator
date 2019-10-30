c6d43607f8662702610253a666590ee1c005485a
thoran <code@thoran.com>
2019-10-30 15:28:37 +1100

    ~ ToyRobotSimulator.Tabletop & test to be much more comprehensive and to fix a logic error.

--- lib/toy_robot_simulator/tabletop.ex
+++ lib/toy_robot_simulator/tabletop.ex
@@ -3,16 +3,16 @@ defmodule ToyRobotSimulator.Tabletop do
   defstruct [x_dimension: 4, y_dimension: 4]
   alias ToyRobotSimulator.Tabletop
 
-  def valid_location?(%{x: x}) do
+  def valid_x_location?(%{x: x}) do
     x >= 0 && x <= %Tabletop{}.x_dimension
   end
 
-  def valid_location?(%{y: y}) do
+  def valid_y_location?(%{y: y}) do
     y >= 0 && y <= %Tabletop{}.y_dimension
   end
 
   def valid_location?(%{x: x, y: y}) do
-    valid_location?(%{x: x}) && valid_location?(%{y: y})
+    valid_x_location?(%{x: x}) and valid_y_location?(%{y: y})
   end
 
 end
--- test/toy_robot_simulator/tabletop_test.exs
+++ test/toy_robot_simulator/tabletop_test.exs
@@ -4,43 +4,129 @@ defmodule ToyRobotSimulator.TabletopTest do
   doctest ToyRobotSimulator.Tabletop
   alias ToyRobotSimulator.Tabletop
 
-  describe "valid_location?() is true" do
-    test "when only x is supplied" do
-      location = %{x: 0}
-      result = location |> Tabletop.valid_location?
-      assert result == true
+  # Single dimension
+
+  describe "valid_x_location?() is true" do
+    test "when only x is supplied at the bottom end of the x dimension" do
+      assert Tabletop.valid_x_location?(%{x: 0}) == true
     end
 
-    test "when only y is supplied" do
-      location = %{y: 0}
-      result = location |> Tabletop.valid_location?
-      assert result == true
+    test "when only x is supplied at the top end of the x dimension" do
+      assert Tabletop.valid_x_location?(%{x: 4}) == true
+    end
+  end
+
+  describe "valid_y_location?()" do
+    test "when only y is supplied at the bottom end of the y dimension" do
+      assert Tabletop.valid_y_location?(%{y: 0}) == true
     end
 
-    test "when both x and y are supplied" do
-      location = %{x: 0, y: 0}
-      result = location |> Tabletop.valid_location?
-      assert result == true
+    test "when only y is supplied at the top end of the y dimension" do
+      assert Tabletop.valid_y_location?(%{y: 4}) == true
+    end
+  end
+
+  describe "valid_location?()" do
+
+    # Bottom left corner
+
+    test "when both x and y are supplied for the bottom left corner" do
+      assert Tabletop.valid_location?(%{x: 0, y: 0}) == true
+    end
+
+    # Bottom right corner
+
+    test "when both x and y are supplied for the bottom right corner" do
+      assert Tabletop.valid_location?(%{x: 4, y: 0}) == true
+    end
+
+    # Top left corner
+
+    test "when both x and y are supplied for the top left corner" do
+      assert Tabletop.valid_location?(%{x: 0, y: 4}) == true
+    end
+
+    # Top right corner
+
+    test "when both x and y are supplied for the top right corner" do
+      assert Tabletop.valid_location?(%{x: 4, y: 4}) == true
     end
   end
 
   describe "valid_location?() is false" do
-    test "when only x is supplied" do
-      location = %{x: 5}
-      result = location |> Tabletop.valid_location?
-      assert result == false
+
+    # Single dimension
+
+    test "when only x is supplied below the bottom end of the x dimension" do
+      assert Tabletop.valid_x_location?(%{x: -1}) == false
     end
 
-    test "when only y is supplied" do
-      location = %{y: 5}
-      result = location |> Tabletop.valid_location?
-      assert result == false
+    test "when only x is supplied above the top end of the x dimension" do
+      assert Tabletop.valid_x_location?(%{x: 5}) == false
     end
 
-    test "when both x and y are supplied" do
-      location = %{x: 5, y: 5}
-      result = location |> Tabletop.valid_location?
-      assert result == false
+    test "when only y is supplied below the bottom end of the y dimension" do
+      assert Tabletop.valid_y_location?(%{y: -1}) == false
+    end
+
+    test "when only y is supplied above the top end of the y dimension" do
+      assert Tabletop.valid_y_location?(%{y: 5}) == false
+    end
+
+    # Bottom left corner
+
+    test "when both x and y are supplied for the left of the bottom left corner" do
+      assert Tabletop.valid_location?(%{x: -1, y: 0}) == false
+    end
+
+    test "when both x and y are supplied for below the bottom left corner" do
+      assert Tabletop.valid_location?(%{x: 0, y: -1}) == false
+    end
+
+    test "when both x and y are supplied for below and to the left of the bottom left corner" do
+      assert Tabletop.valid_location?(%{x: -1, y: -1}) == false
+    end
+
+    # Bottom right corner
+
+    test "when both x and y are supplied for the right of the bottom right corner" do
+      assert Tabletop.valid_location?(%{x: 5, y: 0}) == false
+    end
+
+    test "when both x and y are supplied for below the bottom right corner" do
+      assert Tabletop.valid_location?(%{x: 4, y: -1}) == false
+    end
+
+    test "when both x and y are supplied for below and to the right of the bottom right corner" do
+      assert Tabletop.valid_location?(%{x: 5, y: -1}) == false
+    end
+
+    # Top left corner
+
+    test "when both x and y are supplied for the left of the top left corner" do
+      assert Tabletop.valid_location?(%{x: -1, y: 4}) == false
+    end
+
+    test "when both x and y are supplied for above the top left corner" do
+      assert Tabletop.valid_location?(%{x: 0, y: 5}) == false
+    end
+
+    test "when both x and y are supplied for above and to the left of the top left corner" do
+      assert Tabletop.valid_location?(%{x: -1, y: 5}) == false
+    end
+
+    # Top right corner
+
+    test "when both x and y are supplied for the right of the top right corner" do
+      assert Tabletop.valid_location?(%{x: 5, y: 4}) == false
+    end
+
+    test "when both x and y are supplied for above the top right corner" do
+      assert Tabletop.valid_location?(%{x: 4, y: 5}) == false
+    end
+
+    test "when both x and y are supplied for above and to the right of the top right corner" do
+      assert Tabletop.valid_location?(%{x: 5, y: 5}) == false
     end
   end
 
